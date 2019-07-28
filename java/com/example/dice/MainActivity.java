ab880512d6d3b8066d06e97a1bf7156d81062a0c
Darshan <51724336+ItsKDM@users.noreply.github.com>
2019-07-29 00:23:03 +0530

    Initial Commit

---
+++ java/com/example/dice/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.dice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageviewDice;
+    private Random rng = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageviewDice = findViewById(R.id.image_view_dice);
+        imageviewDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollDice();
+            }
+        });
+    }
+    private void rollDice(){
+        int randomNumber = rng.nextInt(6) + 1;
+
+        switch (randomNumber){
+            case 1:
+                imageviewDice.setImageResource(R.drawable.dice1);
+                break;
+
+            case 2:
+                imageviewDice.setImageResource(R.drawable.dice2);
+                break;
+
+            case 3:
+                imageviewDice.setImageResource(R.drawable.dice3);
+                break;
+
+            case 4:
+                imageviewDice.setImageResource(R.drawable.dice4);
+                break;
+
+            case 5:
+                imageviewDice.setImageResource(R.drawable.dice5);
+                break;
+
+            case 6:
+                imageviewDice.setImageResource(R.drawable.dice6);
+                break;
+
+
+        }
+    }
+}
